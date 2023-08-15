aeec541de70535f70f647f3e7ede65a598b0dd8c
Yangyu Chen <cyy@cyyself.name>
2023-08-16 02:32:10 +0800

    add uart

--- playground/src/playground/DigitalTop.scala
+++ playground/src/playground/DigitalTop.scala
@@ -11,6 +11,7 @@ class DigitalTop(implicit p: Parameters) extends ChipyardSystem
   with testchipip.CanHaveTraceIO // Enables optionally adding trace IO
   with testchipip.CanHavePeripheryBlockDevice // Enables optionally adding the block device
   with testchipip.CanHavePeripheryTLSerial // Enables optionally adding the backing memory and serial adapter
+  with sifive.blocks.devices.uart.HasPeripheryUART // Enables optionally adding the sifive UART
   with icenet.CanHavePeripheryIceNIC // Enables optionally adding the IceNIC for FireSim
   with HasChipyardPRCI // Use Chipyard reset/clock distribution
 {
@@ -19,5 +20,6 @@ class DigitalTop(implicit p: Parameters) extends ChipyardSystem
 
 class DigitalTopModule[+L <: DigitalTop](l: L) extends ChipyardSystemModule(l)
   with testchipip.CanHaveTraceIOModuleImp
+  with sifive.blocks.devices.uart.HasPeripheryUARTModuleImp
   with freechips.rocketchip.util.DontTouch
 // DOC include end: DigitalTop
